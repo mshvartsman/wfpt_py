f0ba09e346642e6f6b62d88ca71368fba398b574
Mike Shvartsman <ms44@princeton.edu>
2016-12-05 10:43:03 -0500

    Update version number and description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='wfpt_py',
-      version='0.1',
-      description='Wiener first passage time distributions using Navarro & Fuss approximation',
+      version='0.2',
+      description='Wiener first passage time distributions and expressions',
       url='http://github.com/mshvartsman/wfpt_py',
       author='Mike Shvartsman',
       author_email='ms44@princeton.edu',
